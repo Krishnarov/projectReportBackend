fe7d46de9b8f77b11c1af11bedc2dafc914139ad
krishna kumar <krishnarovji@gmail.com>
2025-05-21 19:06:15 +0530

    set cors

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -8,9 +8,7 @@ cloudinary.config({
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET
 });
-console.log("cloud_name:", process.env.CLOUDINARY_CLOUD_NAME,
-  "api_key: ",process.env.CLOUDINARY_API_KEY,
- " api_secret: ",process.env.CLOUDINARY_API_SECRET,);
+
 
 export default cloudinary;
 
--- index.js
+++ index.js
@@ -8,11 +8,11 @@ dotenv.config()
 ConnectDB()
 const app =express()
 const PORT = process.env.PORT || 3001;
-// app.use(cors({
-//     origin: "http://localhost:5173",
-//      methods: ["GET", "POST", "PUT", "DELETE"],
-// }))
-app.use(cors())
+app.use(cors({
+    origin: "http://localhost:5173",
+     methods: ["GET", "POST", "PUT", "DELETE"],
+}))
+// app.use(cors())
 app.use(express.json())
 
 // app.use('/',(req,res)=>{
